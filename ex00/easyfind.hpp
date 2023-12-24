3b6991e0ce22a436609f171881ea9dbc29a22594
Ali Aslan <aliaslan_61@outlook.com>
2023-12-24 18:16:59 +0300

    message modified

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: aaslan <aaslan@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 13:03:28 by aaslan            #+#    #+#             */
-/*   Updated: 2023/12/24 13:40:44 by aaslan           ###   ########.fr       */
+/*   Updated: 2023/12/24 17:02:19 by aaslan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ typename T::iterator easyfind(T &container, int value)
 {
 	typename T::iterator valuePosition = std::find(container.begin(), container.end(), value);
 	if (valuePosition == container.end())
-		throw std::runtime_error("Error! -- Value Not Found!");
+		throw std::runtime_error("Value Not Found!");
 
 	return valuePosition;
 }
